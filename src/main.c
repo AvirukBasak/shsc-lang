48d7734e63233aa57e047c35c91e1e1f802748c8
AvirukBasak <avirukbasak@gmail.com>
2023-02-03 17:31:03 +0530

    made use of new error repirting fn

--- src/main.c
+++ src/main.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+#include "globals.h"
+#include "io.h"
 #include "lexer.h"
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
-        fprintf(stderr, "scsh: no args provided\n");
-        exit(2);
-    }
+    if (argc < 2) io_errndie("no args provided");
     for (int i = 1; i < argc; i++) {
+        global_currfile = argv[i];
         FILE *f = fopen(argv[i], "r");
-        if (!f) fprintf(stderr, "scsh: couldn't read file: '%s'\n", argv[i]);
+        if (!f) io_errndie("couldn't read file: '%s'", argv[i]);
         LexToken tok = lex_get_nexttok(f);
         do {
             printf("%s: %s\n", lex_get_tokcode(tok), lex_get_tokstr());
