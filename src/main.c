fc7d17a64e58b47e1a345e6783042f5f339beece
AvirukBasak <avirukbasak@gmail.com>
2023-02-03 17:46:54 +0530

    - as arg reads from stdin

--- src/main.c
+++ src/main.c
@@ -11,14 +11,16 @@ int main(int argc, char **argv)
     if (argc < 2) io_errndie("no args provided");
     for (int i = 1; i < argc; i++) {
         global_currfile = argv[i];
-        FILE *f = fopen(argv[i], "r");
+        FILE *f = (argv[i][0] == '-' && !argv[i][1]) ?
+            stdin :
+            fopen(argv[i], "r");
         if (!f) io_errndie("couldn't read file: '%s'", argv[i]);
         LexToken tok = lex_get_nexttok(f);
         do {
             printf("%s: %s\n", lex_get_tokcode(tok), lex_get_tokstr());
             tok = lex_get_nexttok(f);
         } while (tok != LEX_INVALID);
-        fclose(f);
+        if (f != stdin) fclose(f);
     }
     return 0;
 }
