d857420227faaeeeb549e03a986cf9af31b1115c
AvirukBasak <avirukbasak@gmail.com>
2023-02-06 21:47:34 +0530

    lexer: added support for literals

--- src/lex_match_literals.c.h
+++ src/lex_match_literals.c.h
@@ -3,6 +3,57 @@
 
 LexToken lex_match_literals(FILE *f, char ch)
 {
+    if (ch == '\'') {
+        lex_buffreset();
+        do {
+            ch = getc(f);
+            if (ch == '\\') {
+                lex_buffpush(getc(f));
+                continue;
+            }
+            if (ch == '\'') break;
+            if (ch == (char) EOF) lex_throw("unexpected end of file");
+            lex_buffpush(ch);
+        } while (true);
+        return LEX_CHAR_LITERAL;
+    } else if (ch == '"') {
+        lex_buffreset();
+        do {
+            ch = getc(f);
+            if (ch == '\\') {
+                lex_buffpush(getc(f));
+                continue;
+            }
+            if (ch == '"') break;
+            if (ch == (char) EOF) lex_throw("unexpected end of file");
+            lex_buffpush(ch);
+        } while (true);
+        return LEX_STR_LITERAL;
+    } else if (isdigit(ch) || ch == '+' || ch == '-') {
+        bool neg = false;
+        if (ch == '+' || ch == '-') {
+            if (ch == '-') neg = true;
+            ch = lex_getc(f);
+            if (!isdigit(ch)) {
+                lex_ungetc(&ch, f);
+                return LEX_INVALID;
+            } else lex_ungetc(&ch, f);
+        }
+        bool isfloat = false;
+        do {
+            ch = lex_getc(f);
+            if (ch == '.' && !isfloat) {
+                isfloat = true;
+                ch = lex_getc(f);
+            } else if (ch == '.' && isfloat) {
+                lex_ungetc(&ch, f);
+                return LEX_FLOAT_LITERAL;
+            }
+        } while (isdigit(ch));
+        lex_ungetc(&ch, f);
+        if (isfloat) return LEX_FLOAT_LITERAL;
+        else return LEX_INT_LITERAL;
+    }
     return LEX_INVALID;
 }
 
