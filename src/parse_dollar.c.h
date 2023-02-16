a81b7624ffdefeca34ab25179919d64dd544181f
AvirukBasak <avirukbasak@gmail.com>
2023-02-17 00:24:25 +0530

    adding code for idf init

---
+++ src/parse_dollar.c.h
@@ -0,0 +1,68 @@
+#ifndef PARSE_DOLLAR_CH
+#define PARSE_DOLLAR_CH
+
+void parser_dollar(FILE *f, LexToken tok)
+{
+    switch (tok) {
+        case LEXTOK_DOLLAR: {
+            tok = lex_get_nexttok(f);
+            switch (tok) {
+                case LEXTOK_IDENTIFIER: {
+                    tok = lex_get_nexttok(f);
+                    switch (tok) {
+                        case LEXTOK_ASSIGN: {
+                            tok = lex_get_nexttok(f);
+                            switch (tok) {
+                                case LEXTOK_CHAR_LITERAL: {
+                                    break;
+                                }
+                                case LEXTOK_BINFLOAT_LITERAL: {
+                                    break;
+                                }
+                                case LEXTOK_OCTFLOAT_LITERAL: {
+                                    break;
+                                }
+                                case LEXTOK_DECFLOAT_LITERAL: {
+                                    break;
+                                }
+                                case LEXTOK_HEXFLOAT_LITERAL: {
+                                    break;
+                                }
+                                case LEXTOK_BININT_LITERAL: {
+                                    break;
+                                }
+                                case LEXTOK_OCTINT_LITERAL: {
+                                    break;
+                                }
+                                case LEXTOK_DECINT_LITERAL: {
+                                    break;
+                                }
+                                case LEXTOK_HEXINT_LITERAL: {
+                                    break;
+                                }
+                                case LEXTOK_STR_LITERAL: {
+                                    break;
+                                }
+                                case LEXTOK_INTERP_STR_LITERAL: {
+                                    break;
+                                }
+                                case LEXTOK_IDENTIFIER: {
+                                    break;
+                                }
+                                case LEXTOK_KWD_CALC: {
+                                    break;
+                                }
+                                default: parser_throw("unexpected symbol, expected literal or identifier");
+                            }
+                        }
+                        default: parser_throw("unexpected symbol, expected '='");
+                    }
+                }
+                default: parser_throw("unexpected symbol, expected identifier");
+            }
+        }
+        default: parser_throw("unexpected symbol, expected '$'");
+    }
+}
+
+#endif
--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,8 @@
 #include "lexer.h"
 #include "parser.h"
+#include "io.h"
+
+#include "parse_dollar.c.h"
 
 void parser_interpret(FILE *f)
 {
@@ -9,4 +12,18 @@ void parser_interpret(FILE *f)
         tok = lex_get_nexttok(f);
     }
     printf("%s\n", lex_get_tokcode(tok));
+    return;
+    // new code
+    tok = lex_get_nexttok(f);
+    while (tok != LEXTOK_EOF) {
+        parse_dollar(f, tok);
+        tok = lex_get_nexttok(f);
+    }
+}
+
+void parser_throw(const char *msg)
+{
+    if (!msg) abort();
+    io_print_srcerr(lex_line_no, lex_char_no, "parser error: %s", msg);
+    exit(2);
 }
