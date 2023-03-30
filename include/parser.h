0ec57a54779bb5a33728104c261e4a0fb8cabb7d
AvirukBasak <avirukbasak@gmail.com>
2023-03-30 23:09:53 +0530

    added some code for the variable table

--- include/parser.h
+++ include/parser.h
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+#include "ctl/hashmap.h"
+#include "tokens.yac.h"
+
 extern FILE *yyin;
 
 int yyerror(const char* s);
@@ -16,5 +19,24 @@ int64_t parse_int(const char *str, int base);
 double parse_float(const char *str, int base);
 char *parse_str(const char *str);
 char *parse_interpstr(const char *str);
+
+typedef const char *str_t;
+
+typedef enum {
+    VT_CHR,
+    VT_I64,
+    VT_F64,
+    VT_STR,
+    VT_ANY,
+} VarType;
+
+typedef struct {
+    YYSTYPE var;
+    VarType type;
+} VarData;
+
+HASHMAP_DECLARE(str_t, VarData);
+
+HashMap(str_t, VarData) var_table;
 
 #endif
