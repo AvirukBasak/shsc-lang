2f40a0a8798a890ec0e3bcf3dd2c64226647c698
AvirukBasak <avirukbasak@gmail.com>
2023-07-15 21:23:28 +0530

    code refactor; parse_throw automatically displays error token so no need to sprintf-ing a custom error msg

--- src/parser/parse_bool.c.h
+++ src/parser/parse_bool.c.h
@@ -13,10 +13,7 @@ bool parse_bool(const char *str)
 {
     if (!strcmp(lex_get_buffstr(), "true"))  return true;
     if (!strcmp(lex_get_buffstr(), "false")) return false;
-    char *err_msg = malloc(strlen(str) + GLOBAL_BYTES_BUFFER_LEN);
-    sprintf(err_msg, "invalid boolean literal: '%s'", str);
-    parse_throw(err_msg);
-    free(err_msg);
+    parse_throw("invalid boolean literal");
     return false;
 }
 
--- src/parser/parse_i64.c.h
+++ src/parser/parse_i64.c.h
@@ -12,47 +12,46 @@
 
 int64_t parse_int(const char *str, int base)
 {
-    // Check that the base is valid
-    if (base != 2 && base != 8 && base != 10 && base != 16) {
+    /* check that the base is valid */
+    if (base != 2 && base != 8 && base != 10 && base != 16)
         parse_throw("unsupported base");
-    }
 
-    char *endptr;    // Pointer to the first invalid character in the input string
-    int64_t result;  // The parsed integer value
-    errno = 0;       // Set errno to 0 before calling strtol
+    char *endptr;
+    int64_t result = 0;
 
-    // Handle scientific notation separately
+    /* set errno to 0 before calling strtol */
+    errno = 0;
+
+    /* handle scientific notation separately */
     if (strchr(str, 'e')) {
-        result = strtol(str, &endptr, base);  // Parse the integer part before the 'e'
-        if (errno || *endptr != 'e') {
+        /* parse the integer part before the 'e' */
+        result = strtol(str, &endptr, base);
+        if (errno || *endptr != 'e')
             parse_throw("invalid integer format");
-        }
-        endptr++;  // Skip over the 'e' character
-
+        /* skip over the 'e' character */
+        ++endptr;
         char *exponent_endptr;
-        int64_t exponent = strtol(endptr, &exponent_endptr, 10);  // Parse the exponent value
-        if (errno || exponent_endptr == endptr || *exponent_endptr != '\0') {
+        /* parse the exponent value */
+        int64_t exponent = strtol(endptr, &exponent_endptr, 10);
+        if (errno || exponent_endptr == endptr || *exponent_endptr != '\0')
             parse_throw("invalid integer format");
-        }
-
-        // Check that the resulting value is representable in int64_t
+        /* check that the resulting value is representable in int64_t */
         if (exponent >= 0) {
-            if (exponent > 18 || (result >= INT64_MAX / (int64_t)(1LL << exponent))) {
+            if ( exponent > 18 || (result >= INT64_MAX / (int64_t) (1LL << exponent)) )
                 parse_throw("integer overflow");
-            }
-            result *= (int64_t)(1LL << exponent);  // Multiply by 10^exponent
+            /* multiply by 10^exponent */
+            result *= (int64_t) (1LL << exponent);
         } else {
-            if (exponent < -18 || (result <= INT64_MIN / (int64_t)(1LL << (-exponent)))) {
+            if ( exponent < -18 || (result <= INT64_MIN / (int64_t) (1LL << (-exponent))) )
                 parse_throw("integer underflow");
-            }
-            result /= (int64_t)(1LL << (-exponent));  // Divide by 10^-exponent
+            /* divide by 10^-exponent */
+            result /= (int64_t) (1LL << (-exponent));
         }
     } else {
-        // Parse the integer value using strtol
+        /* parse the integer value using strtol */
         result = strtol(str, &endptr, base);
-        if (errno || *endptr != '\0') {
+        if (errno || *endptr != '\0')
             parse_throw("invalid integer format");
-        }
     }
 
     return result;
