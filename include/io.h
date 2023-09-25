d14537cac76cdaad72a275033aeaffc971434c80
AvirukBasak <avirukbasak@gmail.com>
2023-09-25 23:55:14 +0530

    added docs to io_getline

--- include/io.h
+++ include/io.h
@@ -3,6 +3,11 @@
 
 #include <stddef.h>
 
+/** read string contains trailing new line.
+    size_t n is the number of bytes allocated.
+    return value is the length of string read. */
+ssize_t io_getline(char **lineptr, size_t *n, FILE *stream);
+
 long long io_get_filesize(const char *filepath);
 char **io_read_lines(const char *filepath, size_t *line_cnt);
 void io_errnexit(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
