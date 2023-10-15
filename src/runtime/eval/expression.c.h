919ae396e202dc4897a7dc4cf194543377fffa65
AvirukBasak <avirukbasak@gmail.com>
2023-10-16 00:07:11 +0530

    added operators module

--- src/runtime/eval/expression.c.h
+++ src/runtime/eval/expression.c.h
@@ -130,59 +130,68 @@ rt_eval_Expression_eval_skip_lhs_and_rhs:
 
 rt_eval_Expression_eval_skip_all_3_operands:
     switch (expr->op) {
-        case TOKEN_BANG:
-        case TOKEN_LOGICAL_UNEQUAL:
-        case TOKEN_PERCENT:
-        case TOKEN_MODULO_ASSIGN:
-        case TOKEN_AMPERSAND:
-        case TOKEN_LOGICAL_AND:
-        case TOKEN_LOGICAL_AND_ASSIGN:
-        case TOKEN_BITWISE_AND_ASSIGN:
-        case TOKEN_ASTERIX:
-        case TOKEN_EXPONENT:
-        case TOKEN_EXPONENT_ASSIGN:
-        case TOKEN_MULTIPLY_ASSIGN:
-        case TOKEN_PLUS:
-        case TOKEN_INCREMENT:
+        /* shortcut assignment operators */
         case TOKEN_ADD_ASSIGN:
-        case TOKEN_MINUS:
-        case TOKEN_DECREMENT:
-        case TOKEN_SUBSTRACT_ASSIGN:
-            rt_throw("unimplemented operators");
-            break;
-        case TOKEN_DOT:
-            rt_throw("memebership operator '.' is not yet supported");
-            break;
-        case TOKEN_FSLASH:
-        case TOKEN_FLOOR_DIVIDE:
-        case TOKEN_FLOOR_DIVIDE_ASSIGN:
-        case TOKEN_DIVIDE_ASSIGN:
-        case TOKEN_DCOLON:
-        case TOKEN_LBRACE_ANGULAR:
-        case TOKEN_BITWISE_LSHIFT:
+        case TOKEN_ARITH_RSHIFT_ASSIGN:
+        case TOKEN_BITWISE_AND_ASSIGN:
         case TOKEN_BITWISE_LSHIFT_ASSIGN:
-        case TOKEN_LOGICAL_LESSER_EQUAL:
+        case TOKEN_BITWISE_OR_ASSIGN:
+        case TOKEN_BITWISE_RSHIFT_ASSIGN:
+        case TOKEN_BITWISE_XOR_ASSIGN:
+        case TOKEN_DIVIDE_ASSIGN:
+        case TOKEN_EXPONENT_ASSIGN:
+        case TOKEN_FLOOR_DIVIDE_ASSIGN:
+        case TOKEN_LOGICAL_AND_ASSIGN:
+        case TOKEN_LOGICAL_OR_ASSIGN:
+        case TOKEN_MODULO_ASSIGN:
+        case TOKEN_MULTIPLY_ASSIGN:
+        case TOKEN_SUBSTRACT_ASSIGN:
+        /* remaining operators */
+        case TOKEN_AMPERSAND:
+        case TOKEN_ARITH_RSHIFT:
             rt_throw("unimplemented operators");
             break;
         case TOKEN_ASSIGN:
             rt_VarTable_acc_setval(*rt_VarTable_modf(lhs, *rhs));
             break;
-        case TOKEN_LOGICAL_EQUAL:
-        case TOKEN_RBRACE_ANGULAR:
-        case TOKEN_LOGICAL_GREATER_EQUAL:
+        case TOKEN_ASTERIX:
+        case TOKEN_BANG:
+        case TOKEN_BITWISE_LSHIFT:
         case TOKEN_BITWISE_RSHIFT:
-        case TOKEN_BITWISE_RSHIFT_ASSIGN:
-        case TOKEN_ARITH_RSHIFT:
-        case TOKEN_ARITH_RSHIFT_ASSIGN:
         case TOKEN_CARET:
-        case TOKEN_BITWISE_XOR_ASSIGN:
-        case TOKEN_PIPE:
-        case TOKEN_BITWISE_OR_ASSIGN:
+        case TOKEN_DCOLON:
+        case TOKEN_DECREMENT:
+        case TOKEN_DOT:
+        case TOKEN_EXPONENT:
+        case TOKEN_FLOOR_DIVIDE:
+        case TOKEN_FSLASH:
+        case TOKEN_INCREMENT:
+        case TOKEN_LBRACE_ANGULAR:
+        case TOKEN_LOGICAL_AND:
+        case TOKEN_LOGICAL_EQUAL:
+        case TOKEN_LOGICAL_GREATER_EQUAL:
+        case TOKEN_LOGICAL_LESSER_EQUAL:
         case TOKEN_LOGICAL_OR:
-        case TOKEN_LOGICAL_OR_ASSIGN:
+        case TOKEN_LOGICAL_UNEQUAL:
+        case TOKEN_MINUS:
+        case TOKEN_PERCENT:
+        case TOKEN_PIPE:
+        case TOKEN_PLUS:
+        case TOKEN_RBRACE_ANGULAR:
         case TOKEN_TILDE:
             rt_throw("unimplemented operators");
             break;
+        case TOKOP_FNARGS_INDEXING: {
+            if (!rhs || rhs->type != rt_DATA_TYPE_I64)
+                rt_throw("argument index should evaluate to an `i64`");
+            rt_Data_t args = *rt_VarTable_getref("$");
+            if (args.type != rt_DATA_TYPE_LST)
+                io_errndie("rt_eval_Expression: TOKOP_FNARGS_INDEXING: "
+                           "received arguments list as type '%s'", rt_Data_typename(args));
+            rt_VarTable_acc_setadr(
+                rt_DataList_getref(args.data.lst, rhs->data.i64));
+            break;
+        }
         case TOKOP_FNCALL: {
             if (lhs->type != rt_DATA_TYPE_PROC)
                 rt_throw("cannot make procedure call to type '%s'", rt_Data_typename(*lhs));
@@ -201,23 +210,12 @@ rt_eval_Expression_eval_skip_all_3_operands:
             return;
         }
         case TOKOP_INDEXING:
-        case TOKOP_TERNARY_COND:
             rt_throw("unimplemented operators");
             break;
-        case TOKOP_FNARGS_INDEXING: {
-            if (!rhs || rhs->type != rt_DATA_TYPE_I64)
-                rt_throw("argument index should evaluate to an `i64`");
-            rt_Data_t args = *rt_VarTable_getref("$");
-            if (args.type != rt_DATA_TYPE_LST)
-                io_errndie("rt_eval_Expression: TOKOP_FNARGS_INDEXING: "
-                           "received arguments list as type '%s'", rt_Data_typename(args));
-            rt_VarTable_acc_setadr(
-                rt_DataList_getref(args.data.lst, rhs->data.i64));
-            break;
-        }
         case TOKOP_NOP:
             rt_VarTable_acc_setval(*lhs);
             break;
+        case TOKOP_TERNARY_COND:
         /* stuff that doesn't form an operation */
         default:
             io_errndie("rt_eval_Expression: invalid operation '%s'",
