5d5da6f76a9f0550feca6528d8cb8c9c30fcb19f
AvirukBasak <avirukbasak@gmail.com>
2023-10-19 01:27:31 +0530

    added code in tokop_nop.c.h and tokop_assign.c.h

--- src/runtime/operators/tokop_nop.c.h
+++ src/runtime/operators/tokop_nop.c.h
@@ -1,10 +1,14 @@
 #ifndef RT_OP_TOKOP_NOP_C_H
 #define RT_OP_TOKOP_NOP_C_H
 
+#include "runtime/VarTable.h"
 #include "runtime/data/Data.h"
 #include "runtime/operators.h"
 
-void rt_op_nop(rt_Data_t *lhs, rt_Data_t *rhs) {}
+void rt_op_nop(rt_Data_t *lhs)
+{
+    rt_VarTable_acc_setval(*lhs);
+}
 
 #else
     #warning re-inclusion of module 'runtime/operators/tokop_nop.c.h'
--- src/runtime/operators/tokop_ternary_cond.c.h
+++ src/runtime/operators/tokop_ternary_cond.c.h
@@ -4,7 +4,7 @@
 #include "runtime/data/Data.h"
 #include "runtime/operators.h"
 
-void rt_op_ternary_cond(rt_Data_t *lhs, rt_Data_t *rhs, rt_Data_t *cond) {}
+void rt_op_ternary_cond(rt_Data_t *lhs, rt_Data_t *rhs, rt_Data_t *condition) {}
 
 #else
     #warning re-inclusion of module 'runtime/operators/tokop_ternary_cond.c.h'
