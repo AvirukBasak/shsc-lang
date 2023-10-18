132dec78d93566d2461d2795ef69ac22da8b9923
AvirukBasak <avirukbasak@gmail.com>
2023-10-19 01:03:41 +0530

    moved rt_call_function as rt_op_fncall_handler to operators/tokop_fncall.c.h

--- src/runtime/operators/tokop_fncall.c.h
+++ src/runtime/operators/tokop_fncall.c.h
@@ -1,6 +1,9 @@
 #ifndef OP_TOKOP_FNCALL_C_H
 #define OP_TOKOP_FNCALL_C_H
 
+#include "ast.h"
+#include "ast/api.h"
+#include "functions.h"
 #include "io.h"
 #include "runtime/data/Data.h"
 #include "runtime/data/DataList.h"
@@ -8,6 +11,8 @@
 #include "runtime/io.h"
 #include "runtime/operators.h"
 #include "runtime/VarTable.h"
+
+void rt_op_fncall_handler(const ast_Identifier_t *module, const ast_Identifier_t *proc);
 
 void rt_op_fncall(rt_Data_t *lhs, rt_Data_t *rhs) {
     if (lhs->type != rt_DATA_TYPE_PROC)
@@ -23,10 +28,49 @@ void rt_op_fncall(rt_Data_t *lhs, rt_Data_t *rhs) {
     /* copy fn args into temporary location */
     rt_VarTable_modf(rt_VarTable_getref("$"), *rhs);
     /* get fn code and push code to stack */
-    rt_call_function(lhs->data.proc.modulename, lhs->data.proc.procname);
+    rt_op_fncall_handler(lhs->data.proc.modulename, lhs->data.proc.procname);
     /* set no data to accumulator as data is already set by procedure called above
        return early to prevent accumulator being modified by some other code */
     return;
+}
+
+void rt_op_fncall_handler(const ast_Identifier_t *module, const ast_Identifier_t *proc)
+{
+    /* get code as AST from user defined function */
+    const ast_Statements_t *code = ast_util_ModuleAndProcTable_get_code(module, proc);
+    /* get a descriptor to in-built function */
+    const fn_FunctionDescriptor_t fn = fn_FunctionsList_getfn(
+        module->identifier_name, proc->identifier_name);
+    /* backup metadata on current module and function */
+    const char *currfile_bkp = rt_currfile;
+    const ast_Identifier_t *currmodule_bkp = rt_current_module;
+    const ast_Identifier_t *currproc_bkp = rt_current_proc;
+    /* update metadata to new module and function */
+    if (code) {
+        rt_currfile = ast_util_ModuleAndProcTable_get_filename(module, proc);
+        rt_current_module = module;
+        rt_current_proc = proc;
+    } else if (fn != fn_UNDEFINED) {
+        /* rt_currfile = rt_currfile; */
+        rt_current_module = module;
+        rt_current_proc = proc;
+    }
+    if (code) {
+        /* call user defined function */
+        rt_VarTable_push_proc(proc->identifier_name);
+        rt_eval_Statements(code);
+        rt_VarTable_pop_proc();
+    } else {
+        /* attempt to call in-built function */
+        if (fn == fn_UNDEFINED)
+            rt_throw("undefined procedure '%s:%s'",
+                module->identifier_name, proc->identifier_name);
+        rt_VarTable_acc_setval(fn_FunctionsList_call(fn));
+    }
+    /* restore metadata to previous module and function */
+    rt_currfile = currfile_bkp;
+    rt_current_module = currmodule_bkp;
+    rt_current_proc = currproc_bkp;
 }
 
 #else
