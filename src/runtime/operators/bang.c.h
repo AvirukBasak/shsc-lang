d38be6067b6bd96c2e02518721442b963733998e
AvirukBasak <avirukbasak@gmail.com>
2023-10-21 12:58:16 +0530

    implemented few operators for integral types

--- src/runtime/operators/bang.c.h
+++ src/runtime/operators/bang.c.h
@@ -1,10 +1,22 @@
 #ifndef RT_OP_BANG_C_H
 #define RT_OP_BANG_C_H
 
+#include <stdbool.h>
+
+#include "errcodes.h"
+#include "io.h"
 #include "runtime/data/Data.h"
 #include "runtime/operators.h"
+#include "runtime/VarTable.h"
 
-void rt_op_bang(rt_Data_t *lhs, rt_Data_t *rhs) {}
+void rt_op_bang(rt_Data_t *lhs, rt_Data_t *rhs)
+{
+    if (!lhs) io_errndie("rt_op_bang:" ERR_MSG_NULLPTR " for `lhs`");
+    if (rhs) io_errndie("rt_op_bang: expected `rhs` to be null");
+
+    bool cond = rt_Data_tobool(*lhs);
+    rt_VarTable_acc_setval(rt_Data_bul(!cond));
+}
 
 #else
     #warning re-inclusion of module 'runtime/operators/bang.c.h'
--- src/runtime/operators/logical_or.c.h
+++ src/runtime/operators/logical_or.c.h
@@ -1,10 +1,21 @@
 #ifndef RT_OP_LOGICAL_OR_C_H
 #define RT_OP_LOGICAL_OR_C_H
 
+#include "errcodes.h"
+#include "io.h"
 #include "runtime/data/Data.h"
 #include "runtime/operators.h"
+#include "runtime/VarTable.h"
 
-void rt_op_logical_or(rt_Data_t *lhs, rt_Data_t *rhs) {}
+void rt_op_logical_or(rt_Data_t *lhs, rt_Data_t *rhs)
+{
+    if (!lhs) io_errndie("rt_op_logical_or:" ERR_MSG_NULLPTR " for `lhs`");
+    if (!rhs) io_errndie("rt_op_logical_or:" ERR_MSG_NULLPTR " for `rhs`");
+
+    bool cond1 = rt_Data_tobool(*lhs);
+    bool cond2 = rt_Data_tobool(*rhs);
+    rt_VarTable_acc_setval(rt_Data_bul(cond1 || cond2));
+}
 
 #else
     #warning re-inclusion of module 'runtime/operators/logical_or.c.h'
