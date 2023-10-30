e28c085aa0a7933784ec97455b04077a99d840a1
AvirukBasak <avirukbasak@gmail.com>
2023-10-30 17:37:27 +0530

    added support for . op for map membership

--- src/runtime/operators/dot.c.h
+++ src/runtime/operators/dot.c.h
@@ -4,7 +4,32 @@
 #include "runtime/data/Data.h"
 #include "runtime/operators.h"
 
-void rt_op_dot(const rt_Data_t *lhs, const rt_Data_t *rhs) {}
+void rt_op_dot(const rt_Data_t *lhs, const rt_Data_t *rhs)
+{
+    if (lhs->type != rt_DATA_TYPE_MAP)
+        rt_throw("cannot apply membership on type '%s'", rt_Data_typename(*lhs));
+    switch (lhs->type) {
+        case rt_DATA_TYPE_MAP: {
+            if (!rhs)
+                rt_throw("invalid member name");
+            char *key = rt_Data_tostr(*rhs);
+            rt_VarTable_acc_setadr(
+                rt_DataMap_getref(lhs->data.mp, key));
+            free(key);
+            break;
+        }
+        case rt_DATA_TYPE_BUL:
+        case rt_DATA_TYPE_CHR:
+        case rt_DATA_TYPE_I64:
+        case rt_DATA_TYPE_F64:
+        case rt_DATA_TYPE_INTERP_STR:
+        case rt_DATA_TYPE_ANY:
+        case rt_DATA_TYPE_STR:
+        case rt_DATA_TYPE_LST:
+        case rt_DATA_TYPE_PROC:
+            rt_throw("cannot index type '%s'", rt_Data_typename(*lhs));
+    }
+}
 
 #else
     #warning re-inclusion of module 'runtime/operators/dot.c.h'
