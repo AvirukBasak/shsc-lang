ebb3ef845192fb7ece0fcbcbc6dd005a016d60d9
AvirukBasak <avirukbasak@gmail.com>
2023-10-30 17:14:42 +0530

    identifier resolution: works by resolving variables and then procedures and then built-in fn

--- src/runtime/eval/identifier.c.h
+++ src/runtime/eval/identifier.c.h
@@ -4,6 +4,7 @@
 #include "ast.h"
 #include "ast/api.h"
 #include "runtime/eval.h"
+#include "runtime/functions.h"
 #include "runtime/VarTable.h"
 
 void rt_eval_Identifier(const ast_Identifier_t *identifier)
@@ -12,8 +13,47 @@ void rt_eval_Identifier(const ast_Identifier_t *identifier)
         rt_VarTable_acc_setval(rt_Data_null());
         return;
     }
-    rt_VarTable_acc_setadr(
-        rt_VarTable_getref(identifier->identifier_name));
+    /* get a possible variable reference from VarTable */
+    rt_Data_t *ref = rt_VarTable_getref_errnull(identifier->identifier_name);
+    /* if there's a variable reference, return the reference */
+    if (ref) {
+        rt_VarTable_acc_setadr(ref);
+        return;
+    }
+    /* get code as AST from user defined function */
+    const ast_Statements_t *code = ast_util_ModuleAndProcTable_get_code(
+        rt_VarTable_top_proc()->modulename,
+        identifier
+    );
+    /* if not null, return procedure type data */
+    if (code) {
+        rt_VarTable_acc_setval((rt_Data_t) {
+            .data.proc = {
+                .modulename = rt_VarTable_top_proc()->modulename,
+                .procname = identifier
+            },
+            .type = rt_DATA_TYPE_PROC
+        });
+        return;
+    }
+    /* get a descriptor to in-built function */
+    const rt_fn_FunctionDescriptor_t fn = rt_fn_FunctionsList_getfn(
+        rt_VarTable_top_proc()->modulename->identifier_name,
+        identifier->identifier_name
+    );
+    /* if defined, return procedure type data */
+    if (fn != rt_fn_UNDEFINED) {
+        rt_VarTable_acc_setval((rt_Data_t) {
+            .data.proc = {
+                .modulename = rt_VarTable_top_proc()->modulename,
+                .procname = identifier
+            },
+            .type = rt_DATA_TYPE_PROC
+        });
+        return;
+    }
+    /* if neither variable, procedure or built-in function */
+    rt_throw("undefined identifier '%s'", identifier->identifier_name);
 }
 
 #else
