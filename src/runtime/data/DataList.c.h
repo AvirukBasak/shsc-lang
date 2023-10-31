50c5f22d1c94ead3dacc5add654432d6c02fc1f3
AvirukBasak <avirukbasak@gmail.com>
2023-10-31 15:14:13 +0530

    using the gc module to find circular ref in lists and maps

--- src/runtime/data/DataList.c.h
+++ src/runtime/data/DataList.c.h
@@ -12,6 +12,7 @@
 #include "runtime/data/Data.h"
 #include "runtime/data/DataList.h"
 #include "runtime/data/DataStr.h"
+#include "runtime/data/GarbageColl.h"
 #include "runtime/io.h"
 
 rt_DataList_t *rt_DataList_init()
@@ -44,7 +45,14 @@ void rt_DataList_destroy(rt_DataList_t **ptr)
     /* ref counting */
     --lst->rc;
     if (lst->rc < 0) lst->rc = 0;
-    if (lst->rc > 0) return;
+    if (lst->rc > 0) {
+        /* if rc > 0, check if the data has only cyclic references
+           if so, set rc to 0 to free the data */
+        if (rt_data_GC_has_only_cyclic_refcnt(rt_Data_list(lst))) {
+            lst->rc = 0;
+        }
+        else return;
+    }
     /* free if rc 0 */
     for (int64_t i = 0; i < lst->length; i++)
         rt_Data_destroy(&lst->var[i]);
