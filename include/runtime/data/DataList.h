01076c15b44d86f5c9c7b77d530b047c3515cdb2
AvirukBasak <avirukbasak@gmail.com>
2023-11-08 22:40:45 +0530

    added initializer list macro for rt_DataList_t

--- include/runtime/data/DataList.h
+++ include/runtime/data/DataList.h
@@ -13,6 +13,21 @@ struct rt_DataList_t {
     int64_t rc;
 };
 
+
+/* macro that takes a variable number of rt_Data_t type arguments
+   and calls rt_DataList_append for each item */
+#define rt_DataList_from(...)                               \
+    ({                                                      \
+        rt_Data_t args[] = { __VA_ARGS__ };                 \
+        size_t args_len = sizeof(args) / sizeof(rt_Data_t); \
+        rt_DataList_t *lst = rt_DataList_init();            \
+        for (size_t i = 0; i < args_len; i++) {             \
+            rt_DataList_append(lst, args[i]);               \
+        }                                                   \
+        lst;                                                \
+    })
+
+
 rt_DataList_t *rt_DataList_init();
 int64_t rt_DataList_length(const rt_DataList_t *lst);
 void rt_DataList_increfc(rt_DataList_t *lst);
