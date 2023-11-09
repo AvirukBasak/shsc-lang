2451dfffcfd7c1497f682924f2dea5cbddfd1cf0
AvirukBasak <avirukbasak@gmail.com>
2023-11-10 00:19:23 +0530

    added map concat

--- include/runtime/data/DataMap.h
+++ include/runtime/data/DataMap.h
@@ -23,12 +23,21 @@ struct rt_DataMap_t {
 typedef khiter_t rt_DataMap_iter_t;
 
 rt_DataMap_t *rt_DataMap_init();
+rt_DataMap_t *rt_DataMap_clone(const rt_DataMap_t *mp);
+
 int64_t rt_DataMap_length(const rt_DataMap_t *mp);
+
 void rt_DataMap_increfc(rt_DataMap_t *mp);
 void rt_DataMap_decrefc(rt_DataMap_t *mp);
+
 void rt_DataMap_destroy_circular(rt_DataMap_t **ptr, bool flag);
 void rt_DataMap_destroy(rt_DataMap_t **ptr);
+
 void rt_DataMap_insert(rt_DataMap_t *mp, const char *key, rt_Data_t value);
+void rt_DataMap_del(rt_DataMap_t *mp, const char *key);
+
+void rt_DataMap_concat(const rt_DataMap_t *mp1, const rt_DataMap_t *mp2);
+
 const char *rt_DataMap_getkey_copy(const rt_DataMap_t *mp, const char *key);
 /** unlike rt_DataMap_getref, returns NULL if key not found */
 rt_Data_t *rt_DataMap_getref_errnull(const rt_DataMap_t *mp, const char *key);
@@ -37,7 +46,6 @@ rt_Data_t *rt_DataMap_getref_errnull(const rt_DataMap_t *mp, const char *key);
     `void rt_VarTable_modf(rt_Data_t *dest, rt_Data_t src)`
     on the returned data pointer, that'll take care of reference counts */
 rt_Data_t *rt_DataMap_getref(const rt_DataMap_t *mp, const char *key);
-void rt_DataMap_del(rt_DataMap_t *mp, const char *key);
 char *rt_DataMap_tostr(const rt_DataMap_t *mp);
 
 rt_DataMap_iter_t rt_DataMap_begin(rt_DataMap_t *mp);
