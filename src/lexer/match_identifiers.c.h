87abfafa33e0cc266dc6ea73b583187b2251def8
AvirukBasak <avirukbasak@gmail.com>
2024-03-25 11:12:13 +0530

    added pseudo-submodules support

--- src/lexer/match_identifiers.c.h
+++ src/lexer/match_identifiers.c.h
@@ -6,15 +6,50 @@
 
 lex_Token_t lex_match_identifiers(FILE *f, char ch)
 {
+    bool flag_colon_found = false;
+
     if (!isalpha(ch) && ch != '_') return TOKEN_INVALID;
     while (true) {
         ch = lex_getc(f);
         if (!lex_is_alnumundr(ch)) {
+            /*
+               - Lets ':' be a part of an identifier in certain conditions
+               - allows using module declaration "module Mod1:SubMod1:SubMod2"
+               - previously it would be "module Mod1_SubMod1_SubMod2"
+               - allows calling procedure as "Mod1:SubMod1:SubMod2:procedure()"
+               - previously it would be "Mod1_SubMod1_SubMod2:procedure()"
+            */
+            if (ch == ':') {
+                flag_colon_found = true;
+                continue;
+            }
+
             // unget last non identifier char
             lex_ungetc(&ch, f);
             break;
         }
     }
+
+    /* colon found and current line is not the first line i.e.
+       this identifier is not a module declaration and is in
+       form Mod1:SubMod1:SubMod2:procedure.
+
+       we unget the procedure name so that it may be consumed
+       as a seperate identifier.
+
+       we also unget ':' as ':' is a binary operator that
+       operates with LHS = module name and RHS = procedure name */
+    if (flag_colon_found && lex_line_no > 1) {
+        // unget procedure name till ':'
+        while (ch != ':') {
+            lex_ungetc(&ch, f);
+        }
+        // unget all consecutive ':'
+        while (ch == ':') {
+            lex_ungetc(&ch, f);
+        }
+    }
+
     return TOKEN_IDENTIFIER;
 }
 
